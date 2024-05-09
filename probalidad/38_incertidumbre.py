a939c68f8c2a1b021b11954a31ca96cbe8844163
Alexherogin <ragna.no.gin@gmail.com>
2024-05-08 19:11:30 -0600

    comentar
    se comentaron los programas que que hacia falta

--- probalidad/38_incertidumbre.py
+++ probalidad/38_incertidumbre.py
@@ -1,26 +1,27 @@
 
 import random
 
-def lanzar_dado():
-    dado = random.randint(1,6)
+def lanzar_dado(): # Función para simular el lanzamiento de un dado
+    dado = random.randint(1,6) # Genera un número aleatorio entre 1 y 6 (inclusivos)
+
     return dado
 
-def sumular(tiradas):
+def sumular(tiradas): #Función para simular múltiples tiradas de un dado y calcular la frecuencia de cada resultado
     res ={1:0,2:0,3:0,4:0,5:0,6:0} #
     for _ in range(tiradas):
         resu = lanzar_dado()
-        res[resu]+=1
-        return res
+        res[resu]+=1# Incrementa la frecuencia del resultado obtenido
+        return res # Devuelve el diccionario con las frecuencia
     
-tiradas = 1500
-res = sumular(tiradas)
+tiradas = 1500 # Número de tiradas a simular
+res = sumular(tiradas)# Realiza la simulación
 proba_1 = res [1]/ tiradas
 proba_2 = res [2]/ tiradas
 proba_3 = res [3]/ tiradas
 proba_4 = res [4]/ tiradas 
 proba_5 = res [5]/ tiradas
     
-proba_6 = res [6]/ tiradas
+proba_6 = res [6]/ tiradas # Calcula las probabilidades de cada número
 print(F'de las {tiradas}tiradaz')
 print(f'la probabilidad de que salga el numero 1 es {proba_1}')
 print(f'la probabilidad de que salga el numero 2 es {proba_2}')
--- probalidad/39_probabilidad_a_priori.py
+++ probalidad/39_probabilidad_a_priori.py
@@ -1,11 +1,12 @@
 
 
 def priori(dados):
+    # Solicitar al usuario el número de veces que desea tirar el dado
     numeros = int(input('escoja el numero de veses que desea tirar el dado: '))
-
+# Calcular la probabilidad a priori como 1 dividido entre el número de veces que se tira el dado
     proba_i=1/numeros
     return proba_i
-
+# Solicitar al usuario que elija un número del 1 al 6
 numero_buscado =int(input('escoja un numero del 1 al 6 : '))
 
 proba = priori(numero_buscado)
